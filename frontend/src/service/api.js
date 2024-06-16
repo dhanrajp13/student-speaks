cd0167ff63113fc0f11b53d0535f1e0d630387f5
Dhanraj Patil <dhanraj.patil1081@gmail.com>
2024-06-16 14:26:56 +0530

    some changes

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = "http://localhost:5000";
+const API_URI = "https://student-speaks-back.vercel.app";
 
 // function to login user
 export const loginUser = async (userData) => {
